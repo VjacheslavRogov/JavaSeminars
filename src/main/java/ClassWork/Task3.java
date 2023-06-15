b5e2a9f154288f8ad0fa8760c4c739f3a0f8d86f
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-06-15 15:04:23 +0300

    complited task3

--- src/main/java/ClassWork/Task3.java
+++ src/main/java/ClassWork/Task3.java
@@ -1,4 +1,15 @@
 package ClassWork;
 
+import java.util.Scanner;
+
 public class Task3 {
+    public static void main(String[] args) {
+        //Дана строка. Поменять местами её половины.
+        Scanner sc = new Scanner(System.in);
+        String stringNew = sc.nextLine();
+        String subStr1 = stringNew.substring(0, stringNew.length()/2);
+        String subStr2 = stringNew.substring(stringNew.length()/2);
+        String res = subStr2+subStr1;
+        System.out.println(res);
+    }
 }
