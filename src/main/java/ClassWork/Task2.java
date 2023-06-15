93291883cc219307f692f06a12a8fc09e9c0df85
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-06-15 14:53:50 +0300

    complited task2

--- src/main/java/ClassWork/Task2.java
+++ src/main/java/ClassWork/Task2.java
@@ -1,4 +1,33 @@
 package ClassWork;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        int num1 = sc.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            int num = sc.nextInt();
+            if (isNumberPositive(num1, num)) {
+                count++;
+            }
+            num1 = num;
+        }
+        System.out.println("count = " + count);
+    }
+
+    /**
+     * @param a previous number.
+     * @param b next number.
+     * @return is previous number less than 0 and number
+     * is greater or equal 0
+     * @apiNote Дана последовательность N целых чисел. Найти количество положительных чисел, после которых следует отрицательное число.
+     */
+    private static boolean isNumberPositive(int a, int b) {
+        return a >= 0 && b < 0;
+    }
 }
