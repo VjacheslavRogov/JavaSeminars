d14d1b2da9227c4095be69ceefef5681522b49d4
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-06-15 16:28:32 +0300

    complited hw1

---
+++ src/main/java/ClassWork/Task4.java
@@ -0,0 +1,42 @@
+package ClassWork;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String first = sc.nextLine();
+        String second = sc.nextLine();
+        System.out.println(addBinary(first, second));
+    }
+
+    /**
+     * @param a первая строка
+     * @param b вторая строка
+     * @return двоичная сумма двух строк
+     * @apiNote https://leetcode.com/problems/add-binary/
+     */
+    public static String addBinary(String a, String b) {
+        if (a.length() < b.length()) {
+            addBinary(b, a);
+        }
+        int k = 0;
+        int j = b.length() - 1;
+        String result = "";
+        for (int i = a.length() - 1; i >= 0; i--) {
+            if (a.charAt(i) == '1') {
+                k++;
+            }
+            if (j >= 0 && b.charAt(j) == '1') {
+                k++;
+            }
+            j--;
+            result = k % 2 + result;
+            k /= 2;
+        }
+        if (k == 1) {
+            result = k % 2 + result;
+        }
+        return result;
+    }
+}
--- src/main/java/HomeWork/Hw1.java
+++ src/main/java/HomeWork/Hw1.java
@@ -1,4 +1,20 @@
 package HomeWork;
 
+import java.util.Scanner;
+
 public class Hw1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(reverseWords(s));
+    }
+
+    public static String reverseWords(String s) {
+        String[] str = s.trim().split(" ");
+        String out = "";
+        for (int i = str.length - 1; i > 0; i--) {
+            out += str[i] + " ";
+        }
+        return out + str[0];
+    }
 }
