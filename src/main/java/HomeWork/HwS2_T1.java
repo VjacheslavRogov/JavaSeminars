fd0d18741373c7502132558c63358e5437255682
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-06-19 13:25:51 +0300

    completed hws2_t1

--- src/main/java/HomeWork/HwS2_T1.java
+++ src/main/java/HomeWork/HwS2_T1.java
@@ -1,7 +1,28 @@
 package HomeWork;
 
+import java.util.Scanner;
+
 public class HwS2_T1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("sumPrimeNumbers(n, sc) = " + sumPrimeNumbers(n, sc));
+    }
 
+    /**
+     * @apiNote Дана последовательность N целых чисел. Найти сумму простых чисел.
+     * @param number кол-во чисел
+     * @param in сканер
+     * @return сумма простых чисел
+     */
+    private static int sumPrimeNumbers(int number, Scanner in) {
+        int sum = 0;
+        for (int i = 0; i < number; i++) {
+            int num = in.nextInt();
+            if (num < 10) {
+                sum += num;
+            }
+        }
+        return sum;
     }
 }
--- src/main/java/HomeWork/HwS2_T4.java
+++ src/main/java/HomeWork/HwS2_T4.java
@@ -1,4 +1,7 @@
 package HomeWork;
 
 public class HwS2_T4 {
+    public static void main(String[] args) {
+
+    }
 }
