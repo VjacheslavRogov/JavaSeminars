87064365c29c5a006dd92d30a2234f271a5d8f28
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-06-19 12:15:22 +0300

    added hw

---
+++ src/main/java/HomeWork/Hw3.java
@@ -0,0 +1,7 @@
+package HomeWork;
+
+public class Hw3 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/HomeWork/Hw5.java
@@ -0,0 +1,7 @@
+package HomeWork;
+
+public class Hw5 {
+    public static void main(String[] args) {
+
+    }
+}
