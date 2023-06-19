9f1d537b8c8c1458f3e16f220b01f4ee4f9e9e22
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-06-19 15:21:37 +0300

    completed hws2_t1

--- src/main/java/HomeWork/HwS2_T1.java
+++ src/main/java/HomeWork/HwS2_T1.java
@@ -6,23 +6,44 @@ public class HwS2_T1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println("sumPrimeNumbers(n, sc) = " + sumPrimeNumbers(n, sc));
+        System.out.println("sumPrimeNumbers(n, sc) = " + sumSimple(n, sc));
     }
 
     /**
-     * @apiNote Дана последовательность N целых чисел. Найти сумму простых чисел.
      * @param number кол-во чисел
-     * @param in сканер
+     * @param in     сканер
      * @return сумма простых чисел
+     * @apiNote Дана последовательность N целых чисел. Найти сумму простых чисел.
      */
-    private static int sumPrimeNumbers(int number, Scanner in) {
+    private static int sumSimple(int number, Scanner in) {
         int sum = 0;
         for (int i = 0; i < number; i++) {
             int num = in.nextInt();
-            if (num < 10) {
+            if (checkSimple(num)) {
                 sum += num;
             }
         }
         return sum;
+    }
+
+    /**
+     * @param n проверяемое число
+     * @return простое число или нет
+     * @apiNote проверка на простое число
+     */
+    public static boolean checkSimple(int n) {
+        if (n <= 1)
+            return false;
+        else if (n <= 3)
+            return true;
+        else if (n % 2 == 0 || n % 3 == 0)
+            return false;
+        int num = 5;
+        while (num * num <= n) {
+            if (n % num == 0 || n % (num + 2) == 0)
+                return false;
+            num = num + 6;
+        }
+        return true;
     }
 }
--- src/main/java/HomeWork/HwS2_T3.java
+++ src/main/java/HomeWork/HwS2_T3.java
@@ -1,7 +1,63 @@
 package HomeWork;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class HwS2_T3 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int lengthArr = sc.nextInt();
+        int[] arraySours = createArray(lengthArr, sc);
+        int numReplace = sumIndexTwoDigitNum(arraySours);
+        System.out.println("replacementArray(numReplace) = " + Arrays.toString(replacementArray(numReplace, arraySours)));
+    }
 
+    /**
+     * @param length длинна массива
+     * @param in     сканер
+     * @return заполненный массив
+     * @apiNote создаёт и заполняет массив
+     */
+    public static int[] createArray(int length, Scanner in) {
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = in.nextInt();
+        }
+        return arr;
+    }
+
+    /**
+     * @param array входной массив
+     * @return sum результат вычислений
+     * @apiNote находит сумму индексов двузначных элементов массива
+     */
+    private static int sumIndexTwoDigitNum(int[] array) {
+//        for (int i = 0; i < array.length - 1; i++) {
+//            if (array[i] < 0) {
+//                array[i] *= -1;
+//            }
+//        }
+        int sum = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > 9 && array[i] < 100 || array[i] < -9 && array[i] > -100) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * @param numReplace число для замены
+     * @param arrReplace входной массив
+     * @return arrReplace изменённый массив
+     * @apiNote меняет отрицательные числа на numReplace
+     */
+    private static int[] replacementArray(int numReplace, int[] arrReplace) {
+        for (int i = 0; i < arrReplace.length; i++) {
+            if (arrReplace[i] < 0) {
+                arrReplace[i] = numReplace;
+            }
+        }
+        return arrReplace;
     }
 }
