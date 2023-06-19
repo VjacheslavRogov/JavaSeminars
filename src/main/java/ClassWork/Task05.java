aea2bb60cb419d6ca2947e6f330f8a3ced027e47
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-06-19 10:22:12 +0300

    added task06

--- src/main/java/ClassWork/Task05.java
+++ src/main/java/ClassWork/Task05.java
@@ -1,4 +1,22 @@
 package ClassWork;
 
 public class Task05 {
+    public static void main(String[] args) {
+        int[] myArray = {1,2,3,4,5,4,3,2,1};
+        System.out.println("palindrom(myArray) = " + palindrom(myArray));
+    }
+
+    /**
+     * @apiNote Дан массив целых чисел. Верно ли, что массив является симметричным.Дан массив целых чисел. Верно ли, что массив является симметричным.
+     * @param array входной массив
+     * @return симметричный или нет в виде строки
+     */
+    private static String palindrom(int[] array) {
+        for (int i = 0; i < array.length/2; i++) {
+            if (array[i]!=array[array.length-i-1]){
+                return "Не симметричный!";
+            }
+        }
+        return "Симметричный!";
+    }
 }
