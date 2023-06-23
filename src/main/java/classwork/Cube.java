e2d7d5cf25ee0ed250fcc8f3a24afb2bf015d858
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-06-23 10:51:24 +0300

    completed Task12

---
+++ src/main/java/classwork/Cube.java
@@ -0,0 +1,7 @@
+package classwork;
+
+public class Cube {
+    private Integer size;
+    private String color;
+    private String material;
+}
--- src/main/java/classwork/Task12.java
+++ src/main/java/classwork/Task12.java
@@ -12,21 +12,22 @@ public class Task12 {
         for (int i = 0; i < students.size(); i++) {
             int sumMarks = 0;
 
-            for (int mark: students.get(i).getMarks()){
+            for (int mark : students.get(i).getMarks()) {
                 sumMarks += mark;
             }
-            if (students.get(i).getSurName().endsWith("ова") && sumMarks % 2 == 0){
+            if (students.get(i).getSurName().endsWith("ова") && sumMarks % 2 == 0) {
                 System.out.println(students.get(i).getSalary());
             }
         }
     }
-    public static List<Student> create(){
+
+    public static List<Student> create() {
         List<Student> students = new ArrayList<>();
 
-        students.add(new Student("Иванов", 1, 10_000, List.of(3,4,5)));
-        students.add(new Student("Гаврилова", 2, 20_000, List.of(4,5,5)));
-        students.add(new Student("Соболев", 3, 15_000, List.of(2,4,2)));
-        students.add(new Student("Матросова", 4, 5_000, List.of(3,4,3)));
+        students.add(new Student("Иванов", 1, 10_000, List.of(3, 4, 5)));
+        students.add(new Student("Гаврилова", 2, 20_000, List.of(4, 5, 5)));
+        students.add(new Student("Соболев", 3, 15_000, List.of(2, 4, 2)));
+        students.add(new Student("Матросова", 4, 5_000, List.of(3, 4, 3)));
 
         return students;
     }
