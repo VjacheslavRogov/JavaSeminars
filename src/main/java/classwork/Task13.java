60a92f6d2dcc81454d8efc032b0182788d7d056c
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-06-23 11:03:42 +0300

    completed Task13

--- src/main/java/classwork/Task13.java
+++ src/main/java/classwork/Task13.java
@@ -1,4 +1,35 @@
 package classwork;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task13 {
+    public static void main(String[] args) {
+        Cube cube1 = new Cube(3, "красный", "деревянный");
+        Cube cube2 = new Cube(4, "желтый", "металлический");
+        Cube cube3 = new Cube(5, "зелёный", "металлический");
+        Cube cube4 = new Cube(10, "желтый", "стальной");
+        List<Cube> cubeList = new ArrayList<>();
+        cubeList.add(cube1);
+        cubeList.add(cube2);
+        cubeList.add(cube3);
+        cubeList.add(cube4);
+        System.out.println("cubeList = " + cubeList);
+        Integer k = 0;
+        Integer sumVolume = 0;
+        Integer kWood = 0;
+
+        for (Cube cube : cubeList) {
+            if (cube.getColor().equals("желтый")) {
+                k++;
+                sumVolume += cube.volumeCube();
+            }
+            if (cube.getMaterial().equals("деревянный") && cube.getSize() == 3){
+                kWood++;
+            }
+        }
+        System.out.println("k = " + k);
+        System.out.println("sumVolume = " + sumVolume);
+        System.out.println("kWood = " + kWood);
+    }
 }
