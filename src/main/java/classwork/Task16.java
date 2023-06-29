58ab5a7f2da3d139e34bfd6887b15451b4745baa
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-06-29 23:33:14 +0300

    completed task15

--- src/main/java/classwork/Task16.java
+++ src/main/java/classwork/Task16.java
@@ -1,4 +1,22 @@
 package classwork;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task16 {
+    public static void main(String[] args) {
+        Map<Character,Integer> mapChar = new HashMap<>();
+        String str = "askfgahskfh kjalksdfalksjhfkl lkaskdfalkjsn,asdf afsdf";
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            if (!mapChar.containsKey(str.charAt(i))){
+                mapChar.put(currentChar,1);
+            }else{
+                mapChar.put(currentChar, mapChar.get(currentChar)+1);
+            }
+        }
+        for (Map.Entry<Character, Integer> entity : mapChar.entrySet()) {
+            System.out.println(entity.getKey() + " встретился в тексте : " + entity.getValue() + " раз(а)");
+        }
+    }
 }
