eef18f0f2c8d17e30b4cbe5621f76a72079f58fa
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-07-01 16:56:37 +0300

    completed hws5

---
+++ src/main/java/HomeWork/HwS5_T2.java
@@ -0,0 +1,32 @@
+package HomeWork;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HwS5_T2 {
+    public static void main(String[] args){
+        String text = "Россия идет вперед всей планеты. Планета — это не Россия.";
+        text = text.replaceAll("[-|–|—]|\\p{Punct}", " ");
+        while (text.contains("  ")){
+            text = text.replace("  ", " ");
+        }
+        String[] arrayText = text.split(" ");
+
+        Map<String, Integer> map = getCountWords(arrayText);
+
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+
+    public static Map<String, Integer> getCountWords(String[] array){
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String current : array){
+            int count = map.getOrDefault(current.toLowerCase(), 0);
+            map.put(current.toLowerCase(), ++count);
+        }
+
+        return map;
+    }
+}
